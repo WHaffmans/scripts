db40bde61d5485b405849717f44b68c45688aaaf
Willem Haffmans <willemhaffmans@gmail.com>
2018-06-12 15:23:02 +0200

    clean code

--- FolderIterator.groovy
+++ FolderIterator.groovy
@@ -24,21 +24,27 @@ import com.actelion.research.orbit.imageAnalysis.utils.OrbitHelper
 import java.awt.*
 import java.util.List
 
-OrbitLogAppender.GUI_APPENDER = false;   // no GUI (error) popups
+//Parameters
+topDirPath = 'C:/Users/dev/Desktop/Orbit batch test';
+totalOutputFilename = "/OUTPUT_TOTAL.txt";
+outputFilename = "/OUTPUT.txt";
+classImageFilename = "/OUTPUT.jpg";
+int outputWidth = 200;
+OrbitLogAppender.GUI_APPENDER = false; // no GUI (error) popups
 
-topDirPath = 'C:\\Users\\dev\\Desktop\\Orbit batch test'
+totalOutputFile = new File(topDirPath + totalOutputFilename)
+topDir = new File(topDirPath); 
 
-
-totalOutputFile = new File(topDirPath + "\\OUTPUT_TOTAL.txt")
-def topDir = new File(topDirPath); //wijzig voor top folder
+//Switch to Local Image provider
 if (!DALConfig.isLocalImageProvider()){
-  DALConfig.switchLocalRemoteImageProvider();
-}
+    DALConfig.switchLocalRemoteImageProvider();
+    }
 ip = DALConfig.getImageProvider(); //TODO: heeft dit ook een check nodig?
+
 topDir.eachDir{
-    
     //TODO: resultaat file aanwezig? skip werkt continue in een lambda?
     println "Enter Folder: " + it.path; //print elke folder in de topfolder
+
     //Get current model
 	modelPath = ""
 	it.eachFileMatch ~/Classification met Ex.omo$/, {modelPath = it.path}  //TODO: check en log
@@ -50,46 +56,41 @@ topDir.eachDir{
     rdf = ip.registerFile(new File(imgPath), 1)
     RecognitionFrame rf = new RecognitionFrame(rdf);
 
-
     //Run Classification
-
     println "Start Classification";
-    
     ClassificationResult res = OrbitHelper.Classify(rdf, rf, model, Collections.singletonList(new Point(-1, -1)), -1, null); 
     
+    //Construct resultString resStr
     resStr = imgPath + " :\n";
     for (String name : res.getRatio().keySet()) {
         resStr += (name + ": " + res.getRatio().get(name) + "\n");
     }
     
+    //Print and accumulate results
     println resStr + "\n";
-
-    new File(it.path + "\\OUTPUT.txt").text = resStr;
+    new File(it.path + outputFilename).text = resStr;
     totalOutputFile.append(resStr + '\n');
 
-    /**
-    * Saves a downscaled classification image to disk. Similar to "Tools->Save classification image" functionality.
-    */
-
-    int width = 200;
-    def fn = it.path + "\\OUTPUT.jpg";
+    //Save ClassImage
+    def fn = it.path + classImageFilename;
 
     final TiledImage classImg = rf.getClassImage().getImage();
     OrbitTiledImage2 mainImgTmp = rf.bimg.getImage();
     for (TiledImagePainter tip: rf.bimg.getMipMaps()) {
         // find a good resolution size
-        if (tip.getWidth()>width)
+        if (tip.getWidth()>outputWidth)
             mainImgTmp = tip.getImage();
     }
     final OrbitTiledImage2 mainImg = mainImgTmp;
     ClassImageRenderer renderer = new ClassImageRenderer();
-    int height = (int) (classImg.getHeight() * (width / (double) classImg.getWidth()));
+    int height = (int) (classImg.getHeight() * (outputWidth / (double) classImg.getWidth()));
     println("start saving classification image to disk");
-    BufferedImage bi = renderer.downsample(classImg, mainImg, width, height);
+    BufferedImage bi = renderer.downsample(classImg, mainImg, outputWidth, height);
     println("writing")
     renderer.saveToDisk(bi, fn);
     println "Done with: " + it.path; //print elke folder in de topfolder
     }
+    
 ip.close(); // close image provider connection
 
 
