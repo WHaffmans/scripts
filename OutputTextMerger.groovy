6a069be1f93e7f5ec6c52232c30404ff01d0364e
Willem Haffmans <willemhaffmans@gmail.com>
2018-07-09 22:58:07 +0200

    OutputTextMerger fix

--- OutputTextMerger.groovy
+++ OutputTextMerger.groovy
@@ -4,12 +4,13 @@ import com.actelion.research.orbit.imageAnalysis.utils.OrbitLogAppender
 OrbitLogAppender.GUI_APPENDER = false;   // no GUI (error) popups
 
 //wijzig voor topfolder
-topDirPath = 'C:/Users/dev/Desktop/Orbit batch test'
+topDirPath = ''
 
 totalOutputFile = new File(topDirPath + "/OUTPUT_TOTAL.txt")
 topDir = new File(topDirPath);
 fCount = 0;
 eCount = 0;
+totalOutputFile.text = ""
 
 topDir.eachDir{
 	txtPath = ""
@@ -18,10 +19,10 @@ topDir.eachDir{
 	if(txtPath!=""){
 		res = (new File(txtPath)).getText();
 		totalOutputFile.append(res + '\n');
-		fcount++;
+		fCount++;
 	} else {
 		println "No output file found in folder :" + it.path;
 		eCount++
 	}
 }
-println "Done appending " + fcount + " output files, with " + eCount + " missing files";
+println "Done appending " + fCount + " output files, with " + eCount + " missing files";
